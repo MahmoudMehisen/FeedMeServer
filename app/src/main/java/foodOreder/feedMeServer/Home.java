86e98e5376d4769b9618256200cd6d391c0e060b
mehisen <mahmoud.mehisen@gmail.com>
2019-07-19 10:44:52 +0200

    add category to server

--- app/src/main/java/foodOreder/feedMeServer/Home.java
+++ app/src/main/java/foodOreder/feedMeServer/Home.java
@@ -1,12 +1,18 @@
 package foodOreder.feedMeServer;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,28 +24,52 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
+
+import javax.xml.transform.Result;
 
 import foodOreder.feedMeServer.Common.Common;
 import foodOreder.feedMeServer.Interface.ItemClickListener;
 import foodOreder.feedMeServer.Model.Category;
 import foodOreder.feedMeServer.ViewHolder.MenuViewHolder;
+import info.hoang8f.widget.FButton;
 
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     FirebaseDatabase database;
     DatabaseReference category;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
     TextView txtFullName;
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
     FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
     FirebaseRecyclerOptions<Category> options;
+
+    MaterialEditText edtName;
+    FButton btnUpload,btnSelect;
+
+    Category newCategory;
+    Uri saveUri;
+    private final int PICK_IMAGE_REQUEST=71;
+    DrawerLayout drawer;
 
 
     @Override
@@ -54,9 +84,15 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         database = FirebaseDatabase.getInstance();
         category = database.getReference("Category");
 
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference("images/");
+
+
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -77,9 +113,126 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         layoutManager = new LinearLayoutManager(this);
         recycler_menu.setLayoutManager(layoutManager);
 
-
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
 
         loadMenu();
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Add new Category");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater  = this.getLayoutInflater();
+        View addMenuLayout = inflater.inflate(R.layout.add_new_menu_layout,null);
+
+        edtName = addMenuLayout.findViewById(R.id.editName);
+        btnSelect = addMenuLayout.findViewById(R.id.btnSelect);
+        btnUpload = addMenuLayout.findViewById(R.id.btnUpload);
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+
+
+        alertDialog.setView(addMenuLayout);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                if(newCategory !=null)
+                {
+                    category.push().setValue(newCategory);
+                    Snackbar.make(drawer,"New categroy "+newCategory.getName()+" was added",Snackbar.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        alertDialog.show();
+
+    }
+
+    private void uploadImage() {
+        if(saveUri!=null)
+        {
+            final ProgressDialog mDilaog = new ProgressDialog(this);
+            mDilaog.setMessage("Uploading...");
+            mDilaog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageReference.child("images/"+imageName);
+            imageFolder.putFile(saveUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mDilaog.dismiss();
+                    Toast.makeText(Home.this,"Uploaded !!!",Toast.LENGTH_SHORT).show();
+                    imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            newCategory =new Category(edtName.getText().toString(),uri.toString());
+
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mDilaog.dismiss();
+                    Toast.makeText(Home.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    mDilaog.setMessage("Uploaded "+progress+"%");
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data !=null && data.getData()!=null )
+        {
+            saveUri = data.getData();
+            btnSelect.setText("Image Selected");
+
+        }
+    }
+
+    private void chooseImage() {
+
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+
+
     }
 
     private void loadMenu() {
