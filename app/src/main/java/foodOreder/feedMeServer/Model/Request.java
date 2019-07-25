d41255fbb04533ae17dfb2c4d3c79935a5ba247a
mehisen <mahmoud.mehisen@gmail.com>
2019-07-25 14:07:35 +0200

    update request

--- app/src/main/java/foodOreder/feedMeServer/Model/Request.java
+++ app/src/main/java/foodOreder/feedMeServer/Model/Request.java
@@ -10,18 +10,21 @@ public class Request {
     private String total;
     private String status;
     private String comment;
-    private List<Order>foods;
+    private String paymentState;
+    private List<Order> foods;
 
     public Request() {
     }
 
-    public Request(String phone, String name, String address, String total, String status, String comment, List<Order> foods) {
+
+    public Request(String phone, String name, String address, String total, String status, String comment, String paymentState, List<Order> foods) {
         this.phone = phone;
         this.name = name;
         this.address = address;
         this.total = total;
         this.status = status;
         this.comment = comment;
+        this.paymentState = paymentState;
         this.foods = foods;
     }
 
@@ -71,6 +74,14 @@ public class Request {
 
     public void setComment(String comment) {
         this.comment = comment;
+    }
+
+    public String getPaymentState() {
+        return paymentState;
+    }
+
+    public void setPaymentState(String paymentState) {
+        this.paymentState = paymentState;
     }
 
     public List<Order> getFoods() {
