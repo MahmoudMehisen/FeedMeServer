3b4160374336183ec96b5f82b81214b7b649fd1f
mehisen <mahmoud.mehisen@gmail.com>
2019-07-25 16:11:30 +0200

    fix notification

---
+++ app/src/main/java/foodOreder/feedMeServer/Helper/NotificationHelper.java
@@ -0,0 +1,59 @@
+package foodOreder.feedMeServer.Helper;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Build;
+
+import foodOreder.feedMeServer.R;
+
+public class NotificationHelper extends ContextWrapper {
+
+    private static final String FEED_ME_CHANEL_ID="foodOreder.feedMeServer";
+
+    private static final String FEED_ME_CHANEL_NAME="Feed Me";
+
+    private NotificationManager manager;
+
+
+    public NotificationHelper(Context base) {
+        super(base);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+        {
+            createChannel();
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel fmChannel = new NotificationChannel(FEED_ME_CHANEL_ID,FEED_ME_CHANEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
+        fmChannel.enableLights(true);
+        fmChannel.enableVibration(true);
+        fmChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManger().createNotificationChannel(fmChannel);
+
+    }
+
+    public NotificationManager getManger() {
+        if(manager == null)
+        {
+            manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+    @TargetApi(Build.VERSION_CODES.O)
+    public Notification.Builder getFeedMeChannelNotification(String title, String body, PendingIntent contentIntent, Uri soundUri){
+        return new Notification.Builder(getApplicationContext(),FEED_ME_CHANEL_ID)
+                .setContentIntent(contentIntent)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(soundUri)
+                .setAutoCancel(false);
+    }
+}
--- app/src/main/java/foodOreder/feedMeServer/Service/MyFirebaseMessaging.java
+++ app/src/main/java/foodOreder/feedMeServer/Service/MyFirebaseMessaging.java
@@ -7,11 +7,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Random;
+
+import foodOreder.feedMeServer.Common.Common;
+import foodOreder.feedMeServer.Helper.NotificationHelper;
 import foodOreder.feedMeServer.MainActivity;
+import foodOreder.feedMeServer.OrderStatus;
 import foodOreder.feedMeServer.R;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
@@ -20,7 +26,30 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        sendNotification(remoteMessage);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            sendNotificationAPI26(remoteMessage);
+        else
+            sendNotification(remoteMessage);
+    }
+
+    private void sendNotificationAPI26(RemoteMessage remoteMessage) {
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        String title = notification.getTitle();
+        String content =notification.getBody();
+
+        Intent intent = new Intent(this, OrderStatus.class);
+        intent.putExtra("userPhone", Common.currentUser.getPhone());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationHelper helper = new NotificationHelper(this);
+        Notification.Builder builder = helper.getFeedMeChannelNotification(title,content,pendingIntent,defaultSoundUri);
+
+        helper.getManger().notify(new Random().nextInt(),builder.build());
+
+
+
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
