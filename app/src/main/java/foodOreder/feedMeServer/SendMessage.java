7acb1bc223a7372186d942f164bf41e04d529fc2
mehisen <mahmoud.mehisen@gmail.com>
2019-07-28 02:35:39 +0200

    fix notification

--- app/src/main/java/foodOreder/feedMeServer/SendMessage.java
+++ app/src/main/java/foodOreder/feedMeServer/SendMessage.java
@@ -1,16 +1,28 @@
 package foodOreder.feedMeServer;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import foodOreder.feedMeServer.Common.Common;
+import foodOreder.feedMeServer.Model.DataMessage;
 import foodOreder.feedMeServer.Model.MyResponse;
 import foodOreder.feedMeServer.Model.Notification;
 import foodOreder.feedMeServer.Model.Sender;
+import foodOreder.feedMeServer.Model.Token;
 import foodOreder.feedMeServer.Remote.APIService;
 import info.hoang8f.widget.FButton;
 import retrofit2.Call;
@@ -38,26 +50,29 @@ public class SendMessage extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Create message
-                Notification notification = new Notification(edtTitle.getText().toString(),edtMessage.getText().toString());
-
-                Sender toTopic =new Sender();
-                toTopic.to = new StringBuilder("/topics/").append(Common.topicName).toString();
-                toTopic.notification = notification;
-
-                mService.sendNotification(toTopic)
-                        .enqueue(new Callback<MyResponse>() {
+                DatabaseReference tokens = FirebaseDatabase.getInstance().getReference("Tokens");
+                tokens.orderByChild("serverToken").equalTo(false)
+                        .addValueEventListener(new ValueEventListener() {
                             @Override
-                            public void onResponse(Call<MyResponse> call, Response<MyResponse> response) {
-                                if(response.isSuccessful())
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                for(DataSnapshot postSnapShot:dataSnapshot.getChildren())
                                 {
-                                    Toast.makeText(SendMessage.this,"Message Sent",Toast.LENGTH_SHORT).show();
+                                    Token token = postSnapShot.getValue(Token.class);
+
+                                    Map<String,String> dataSend = new HashMap<>();
+                                    dataSend.put("title",edtTitle.getText().toString());
+                                    dataSend.put("message",edtMessage.getText().toString());
+                                    DataMessage dataMessage = new DataMessage(token.getToken(),dataSend);
+
+                                    mService.sendNotification(dataMessage);
+
                                 }
                             }
 
                             @Override
-                            public void onFailure(Call<MyResponse> call, Throwable t) {
-                                    Toast.makeText(SendMessage.this,""+t.getMessage(),Toast.LENGTH_SHORT).show();
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
                             }
                         });
             }
